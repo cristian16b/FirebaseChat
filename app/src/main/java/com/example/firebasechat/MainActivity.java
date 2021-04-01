fb4c97f97cd9822097d61ab31e30c4a28e2df1e1
cristian16b <cristian16b>
2021-03-31 21:53:35 -0300

    primer commit

---
+++ app/src/main/java/com/example/firebasechat/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.firebasechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// referencia: https://www.universoandroidhn.com/2018/05/android-message-chat-firebase-example.html
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
