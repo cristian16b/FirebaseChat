cd2810d256600729fab7405e49cd0d34b679e6df
cristian16b <cristian16b>
2021-04-05 23:21:24 -0300

    Agrego el metodo para enviar los datos a firebase

--- app/src/main/java/com/example/firebasechat/ChatActivity.java
+++ app/src/main/java/com/example/firebasechat/ChatActivity.java
@@ -1,13 +1,22 @@
 package com.example.firebasechat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ChatActivity extends AppCompatActivity {
 
     private TextView mTextView;
+
+    private FirebaseListAdapter<ChatMessage> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,29 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         mTextView = (TextView) findViewById(R.id.text);
+
+        Button fab = (Button) findViewById(R.id.fab);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
     }
 }
